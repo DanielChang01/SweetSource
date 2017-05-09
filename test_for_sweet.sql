3d903ba8f6e17759896337d8b0d929aa4a3f201f
yidong.zhang <yidong.zhang@wormpex.com>
2017-05-09 18:04:34 +0800

    chenge sql

--- test_for_sweet.sql
+++ test_for_sweet.sql
@@ -9,11 +9,22 @@
  Target Server Version : 50718
  File Encoding         : utf-8
 
- Date: 05/05/2017 19:24:08 PM
+ Date: 05/09/2017 18:03:40 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `device_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `device_info`;
+CREATE TABLE `device_info` (
+  `user_mac_addr` varchar(255) NOT NULL,
+  `user_ip` varchar(255) NOT NULL,
+  `user_mobile` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_mac_addr`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 --  Table structure for `dish`
@@ -35,7 +46,7 @@ CREATE TABLE `dish` (
 --  Records of `dish`
 -- ----------------------------
 BEGIN;
-INSERT INTO `dish` VALUES ('1', '2017/5/5', '西红柿超鸡蛋', 'i like it.', null, null, null, null);
+INSERT INTO `dish` VALUES ('1', '2017/5/5', '西红柿超鸡蛋', 'i like it.', '甜食', null, '10', null);
 COMMIT;
 
 -- ----------------------------
@@ -63,19 +74,20 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `user_id` int(25) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户编号',
-  `user_name` varchar(50) NOT NULL DEFAULT '''''' COMMENT '用户名',
+  `user_id` int(25) NOT NULL COMMENT '用户编号',
+  `user_name` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
   `user_pass` varchar(20) NOT NULL DEFAULT '''''' COMMENT '密码',
   `user_mobile` varchar(20) NOT NULL DEFAULT '''''',
   `user_shop_id` varchar(30) DEFAULT NULL COMMENT '店铺编号',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`user_mobile`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 --  Records of `users`
 -- ----------------------------
 BEGIN;
-INSERT INTO `users` VALUES ('1', 'sweet', '123', '\'\'', null), ('2', 'sweetshop', '123', '\'\'', '2017/5/5');
+INSERT INTO `users` VALUES ('2', 'sweetshop', '123', '\'\'', '2017/5/5'), ('1', 'sweet', '123', '136', null);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
