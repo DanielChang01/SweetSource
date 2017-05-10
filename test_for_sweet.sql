795dbff1de57417f19025be306c46439da8242cd
yidong.zhang <yidong.zhang@wormpex.com>
2017-05-10 10:01:55 +0800

    convert sql

--- test_for_sweet.sql
+++ test_for_sweet.sql
@@ -9,7 +9,7 @@
  Target Server Version : 50718
  File Encoding         : utf-8
 
- Date: 05/09/2017 18:03:40 PM
+ Date: 05/10/2017 10:00:06 AM
 */
 
 SET NAMES utf8;
@@ -20,11 +20,23 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `device_info`;
 CREATE TABLE `device_info` (
-  `user_mac_addr` varchar(255) NOT NULL,
-  `user_ip` varchar(255) NOT NULL,
-  `user_mobile` varchar(255) NOT NULL,
+  `user_mac_addr` varchar(150) NOT NULL,
+  `user_ip` varchar(50) NOT NULL,
+  `user_mobile` varchar(20) NOT NULL,
   PRIMARY KEY (`user_mac_addr`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户设备信息表，用于定位用户IP，发送信息';
+
+-- ----------------------------
+--  Table structure for `discount`
+-- ----------------------------
+DROP TABLE IF EXISTS `discount`;
+CREATE TABLE `discount` (
+  `user_mobile` varchar(20) NOT NULL,
+  `shop_id` varchar(30) NOT NULL,
+  `consume_times` int(11) NOT NULL COMMENT '消费次数',
+  `consume_sum` decimal(10,0) NOT NULL COMMENT '消费总金额',
+  PRIMARY KEY (`user_mobile`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='店铺消费信息表';
 
 -- ----------------------------
 --  Table structure for `dish`
@@ -40,14 +52,19 @@ CREATE TABLE `dish` (
   `dish_pre_price` decimal(10,0) unsigned DEFAULT NULL COMMENT '现价',
   `dish_cut_price` decimal(10,0) DEFAULT NULL COMMENT '折后价',
   PRIMARY KEY (`dish_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='菜品表';
 
 -- ----------------------------
---  Records of `dish`
+--  Table structure for `orders`
 -- ----------------------------
-BEGIN;
-INSERT INTO `dish` VALUES ('1', '2017/5/5', '西红柿超鸡蛋', 'i like it.', '甜食', null, '10', null);
-COMMIT;
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `order_id` varchar(30) NOT NULL COMMENT '订单编号，格式：order+日期',
+  `order_from_mobile` varchar(20) NOT NULL COMMENT '用户',
+  `order_to_mobile` varchar(20) NOT NULL COMMENT '订单去向',
+  `order_money` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单表，用于记录订单双方信息';
 
 -- ----------------------------
 --  Table structure for `shop`
@@ -60,34 +77,19 @@ CREATE TABLE `shop` (
   `shop_pic` varchar(100) NOT NULL DEFAULT '''''',
   `shop_point` text COMMENT '定位信息，格式：经度，纬度',
   PRIMARY KEY (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
---  Records of `shop`
--- ----------------------------
-BEGIN;
-INSERT INTO `shop` VALUES ('2107/5/5', '饭小二', '大宝路SOD号', '', null);
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='店铺表';
 
 -- ----------------------------
 --  Table structure for `users`
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `user_id` int(25) NOT NULL COMMENT '用户编号',
   `user_name` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
   `user_pass` varchar(20) NOT NULL DEFAULT '''''' COMMENT '密码',
   `user_mobile` varchar(20) NOT NULL DEFAULT '''''',
-  `user_shop_id` varchar(30) DEFAULT NULL COMMENT '店铺编号',
+  `user_shop_id` varchar(30) DEFAULT NULL COMMENT '店铺编号，格式：shop+日期',
   PRIMARY KEY (`user_mobile`),
   UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- ----------------------------
---  Records of `users`
--- ----------------------------
-BEGIN;
-INSERT INTO `users` VALUES ('2', 'sweetshop', '123', '\'\'', '2017/5/5'), ('1', 'sweet', '123', '136', null);
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
 
 SET FOREIGN_KEY_CHECKS = 1;
